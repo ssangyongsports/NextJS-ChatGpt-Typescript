ff226c3171023f3a352b34a59059e84d8e3a8da7
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-05 18:57:09 +0800

    Update get-answer.ts

--- src/pages/api/get-answer.ts
+++ src/pages/api/get-answer.ts
@@ -2,7 +2,9 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_AI_KEY,
+   url: 'https://api.pawan.krd/v1',
+   apiKey: process.env.OPEN_AI_KEY,
+ 
 });
 
 const openai = new OpenAIApi(configuration);
